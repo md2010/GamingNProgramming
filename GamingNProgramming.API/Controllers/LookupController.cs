6bee65f06c10fd740ae406bb6fc4d1a3ce70ab88
md <mdamjanovic2010@gmail.com>
2023-07-02 14:37:10 +0200

    lookups

---
+++ GamingNProgramming.API/Controllers/LookupController.cs
@@ -0,0 +1,51 @@
+﻿using GamingNProgramming.Model;
+using GamingNProgramming.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamingNProgramming.WebAPI.Controllers
+{
+    [Route("api/lookup")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        protected IRoleRepository Repository { get; set; }
+
+        protected IAvatarRepository AvatarRepository { get; set; }
+        public LookupController(IRoleRepository repository, IAvatarRepository repo) 
+        {
+            Repository = repository;   
+            AvatarRepository = repo;
+        }
+
+        [HttpGet]
+        [Route("roles")]
+        public async Task<IActionResult> GetRoles()
+        {
+            var result = await Repository.GetAllAsync();
+            if(result != null && result.Any())
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpGet]
+        [Route("avatars")]
+        public async Task<IActionResult> GetAvatars()
+        {
+            var result = await AvatarRepository.GetAllAsync();
+            if (result != null && result.Any())
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+        
+    }
+}
