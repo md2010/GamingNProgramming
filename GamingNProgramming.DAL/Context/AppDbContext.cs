0a7b18c9ad4a29b9eb6dd8c71ec9fd1676b7b9b9
md <mdamjanovic2010@gmail.com>
2023-07-02 14:36:53 +0200

    DAL

---
+++ GamingNProgramming.DAL/Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using GamingNProgramming.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Configuration;
+
+namespace GamingNProgramming.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbSet<CoreUser> CoreUsers { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Friend> Friends { get; set; }
+        public DbSet<Avatar> Avatars { get; set; }
+
+        public AppDbContext() {}
+        public AppDbContext(IConfiguration configuration, DbContextOptions<AppDbContext> options) : base(options)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-7LG8UG8;Initial Catalog=GamingNProgrammingDB;Integrated Security=True; Trust Server Certificate = True");
+        }
+       
+    }
+}
---
+++ GamingNProgramming.DAL/Migrations/20230701084714_InitialCreate.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamingNProgramming.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Avatars",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avatars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professors",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professors", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CoreUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoreUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CoreUsers_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    XPs = table.Column<int>(type: "int", nullable: false),
+                    ProfessorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    AvatarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Players_Avatars_AvatarId",
+                        column: x => x.AvatarId,
+                        principalTable: "Avatars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_CoreUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "CoreUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_Professors_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professors",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friends",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Player1Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Player2Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friends", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Friends_Players_Player1Id",
+                        column: x => x.Player1Id,
+                        principalTable: "Players",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Friends_Players_Player2Id",
+                        column: x => x.Player2Id,
+                        principalTable: "Players",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CoreUsers_RoleId",
+                table: "CoreUsers",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_Player1Id",
+                table: "Friends",
+                column: "Player1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_Player2Id",
+                table: "Friends",
+                column: "Player2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_AvatarId",
+                table: "Players",
+                column: "AvatarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_ProfessorId",
+                table: "Players",
+                column: "ProfessorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friends");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Avatars");
+
+            migrationBuilder.DropTable(
+                name: "CoreUsers");
+
+            migrationBuilder.DropTable(
+                name: "Professors");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
