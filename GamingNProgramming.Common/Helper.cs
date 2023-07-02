ec518ef1f3ac5845436d12ccf75e1d4ff0c7829a
md <mdamjanovic2010@gmail.com>
2023-07-02 14:37:24 +0200

    helper class

---
+++ GamingNProgramming.Common/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingNProgramming.Common
+{
+    public static class Helper
+    {
+        public static Guid TransformGuid(string guid)
+        {
+            return Guid.Parse(guid);
+        }
+    }
+}
